4e9e31f34b3acac756343b3b9fa928a0077be144
aryabhattammc2023 <145563353+aryabhattammc2023@users.noreply.github.com>
2023-09-20 17:53:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,11 @@
-
-
 document.addEventListener('DOMContentLoaded', function () {
     const complaintForm = document.getElementById('complaintForm');
     const message = document.getElementById('message');
 
     complaintForm.addEventListener('submit', function (e) {
         e.preventDefault();
-        
-        // Get user input
-        const name = document.getElementById('name').value;
-        const phone = document.getElementById('phone').value;
-        const complaint = document.getElementById('complaint').value;
+
         const submit = document.getElementById('submitButton');
-        
-        // Get the current date
-        const currentDate = new Date();
-        const dateString = currentDate.toLocaleDateString();
         
         // Display a success message
         message.textContent = 'Complaint submitted successfully. Thank you!';
@@ -26,7 +16,7 @@ document.addEventListener('DOMContentLoaded', function () {
             message.classList.add('hidden');
             submit.style.marginBottom = defmargin;
         }, 3000); // Hide the message after 3 seconds
-
+        
         // Clear the form
         complaintForm.reset();
     });
